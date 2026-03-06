486e0c9f574630f929d57a23651dd22f6f332c78
nafis ahamed <nafisahamed14@gmail.com>
2026-03-06 17:40:02 +0600

    solid principle

--- obj/Debug/net8.0/solid.AssemblyInfo.cs
+++ obj/Debug/net8.0/solid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("solid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72beb2ab6226dd63d0fc5a0565edf6ed8f51e34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c59a27b4751896f953e6c81106b7a14a89da2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("solid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("solid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
